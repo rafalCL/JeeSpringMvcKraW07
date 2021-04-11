a96f9d83e511a95d7e432e6b353da82625c41467
Rafal <rafal.opial@coderslab.pl>
2021-04-11 11:06:21 +0200

    m7d4t1 configure project

---
+++ src/main/java/pl/coderslab/jeespringmvc/app/AppConfig.java
@@ -0,0 +1,21 @@
+package pl.coderslab.jeespringmvc.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@ComponentScan("pl.coderslab.jeespringmvc")
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+}
